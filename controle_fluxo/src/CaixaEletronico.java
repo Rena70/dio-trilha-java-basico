7c8c67f4e893e907d941460ed84eb4190a542844
Rena70 <renatomoraesr@gmail.com>
2023-06-22 18:54:54 -0300

    sixtaxe de if_elseif_else

--- controle_fluxo/src/CaixaEletronico.java
+++ controle_fluxo/src/CaixaEletronico.java
@@ -1,12 +1,16 @@
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         
-        double saldo = 25.0;
-        double valor_solicitado = 17.0;
+        double saldo = 27.0;
+        double valor_solicitado = 10.0;
         
-        if(valor_solicitado < saldo)
+        if(valor_solicitado <= saldo){
             saldo = saldo - valor_solicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }        
+        else
+            System.out.println("Saldo insuficiente");    
 
-        System.out.println(saldo);
+        
     }
 }
